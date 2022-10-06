7a056892ef2caa55fb2ccd16ab777a7666df2a82
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2022-10-06 17:31:26 +0200

    sorting algo done

--- Department.java
+++ Department.java
@@ -1,8 +1,9 @@
-import java.util.LinkedList; 
+import java.util.*; 
 public class Department {
 
 
     private String department;  
+    private ArrayList[] combo;
     private LinkedList<Team> teamList = new LinkedList<Team>(); // list of teams in the department
 
     /**
@@ -71,5 +72,105 @@ public class Department {
         return teamList.toArray(new Team[teamList.size()]);
     }
     
+    /**
+     * Method to pr
+     * 
+     * @param arr
+     * @return 
+     */
+    public ArrayList<String> comboWombo(LinkedList<String>[] arr) {
+        // Number of arrays
+        int n = arr.length;
+
+        ArrayList<String> out = new ArrayList<String>();
+        
+
+        // To keep track of next element in
+        // each of the n arrays
+        int[] indices = new int[n];
+
+        // Initialize with first element's index
+        for (int i = 0; i < n; i++)
+            indices[i] = 0;
+
+
+        while (true) {
+
+            // For every combination, add the first element of each array to the arraylist
+            ArrayList<String> temp = new ArrayList<String>();
+            for (int i = 0; i < n; i++) {
+                // if the array already contains the element, remove the list 
+                if (temp.contains(arr[i].get(indices[i]))) {
+                    break;
+                } 
+                temp.add(arr[i].get(indices[i]));
+                
+            }
+            // Add the combination to the arraylist if it is not already there
+            // If n-3 then three is missing and so on
+            if (temp.size() >= n) {
+                out.add(temp.toString());
+            }
+
+            // Find the rightmost array that has more
+            // elements left after the current element
+            // in that array
+            int next = n - 1;
+            while (next >= 0 &&
+                    (indices[next] + 1 >= arr[next].size()))
+                next--;
+
+            // No such array is found so no more
+            // combinations left
+            if (next < 0)
+                return out;
+
+            // If found move to next element in that
+            // array
+            indices[next]++;
+
+            // For all arrays to the right of this
+            // array current index again points to
+            // first element
+            for (int i = next + 1; i < n; i++)
+                indices[i] = 0;
+        }
+    }
+
+    /**
+     * Method to feed the comboWombo method with the team lists
+     */
+    public ArrayList<String> getCombo() {
+        // get the number of stations in the department
+        int numStations = 0;
+        
+        for (int i = 0; i < this.getTeamList().length; i++) {
+            numStations += this.getTeamList()[i].getStationList().length; 
+        }
+        LinkedList<String> []arr = new LinkedList[numStations];
+        System.out.println(numStations);
+        
+        for (int i = 0; i < numStations; i++) {
+            // make a new linked list for each station
+            arr[i] = new LinkedList<String>();
+
+        }
+
+        // loop through every fitter and add them to the linked list if the competency is matched
+        for (int i = 0; i < this.getTeamList().length; i++) {
+            for (int j = 0; j < this.getTeamList()[i].getStationList().length; j++) {
+                for (int k = 0; k < this.getTeamList()[i].getFitterList().length; k++) {
+                    for (int l = 0; l < this.getTeamList()[i].getFitterList()[k].getCompetency().length; l++) {
+                        if (this.getTeamList()[i].getStationList()[j] == this.getTeamList()[i].getFitterList()[k].getCompetency()[l]) {
+                            arr[j].add(this.getTeamList()[i].getFitterList()[k].getName());
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        return comboWombo(arr);
+    }
+
 }
- 
